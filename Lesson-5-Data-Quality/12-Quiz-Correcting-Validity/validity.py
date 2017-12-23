4bd3c3f7ea0a96450b60d794d47e3809c1157977
AmanLP7 <ak547276@gmail.com>
2017-12-23 17:17:46 +0530

    Completed code

--- Lesson-5-Data-Quality/12-Quiz-Correcting-Validity/validity.py
+++ Lesson-5-Data-Quality/12-Quiz-Correcting-Validity/validity.py
@@ -29,14 +29,21 @@ def process_file(input_file, output_good, output_bad):
         reader = csv.DictReader(f)
         header = reader.fieldnames
 
-        goodwriter = csv.DictWriter(og,delimeter = ",",fieldnames = header)
-        badwriter = csv.DictWriter(ob,delimeter = ",", fieldnames = header)
+        goodwriter = csv.DictWriter(og,delimiter = ",",fieldnames = header)
+        badwriter = csv.DictWriter(ob,delimiter = ",", fieldnames = header)
 
-        
+        for row in reader:
+            if row['URI'].find("dbpedia") > -1:
+                try:
+                    row['productionStartYear'] = int(row['productionStartYear'][:4])
+                    if row['productionStartYear'] in range(1886,2015):
+                        goodwriter.writerow(row)
+                    else:
+                        badwriter.writerow(row)
+                except ValueError:
+                    badwriter.writerow(row)
 
-        
 
-    
 
 def test():
 
